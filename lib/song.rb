0575ad8b50b757a50cfe7f057cc637810abc3f5e
Alexander <acepetrov96@gmail.com>
2020-11-03 13:07:28 -0600

    commit tree cccea7c52341a8b32c1c515a31f20c221794fee2

---
+++ lib/song.rb
@@ -0,0 +1,56 @@
+class Song
+    @@count = 0
+    @@artists = []
+    @@genres = []
+
+    attr_accessor :name, :artist, :genre
+    attr_reader :genre_count
+
+    def initialize(name, artist, genre)
+        @name = name 
+        @artist = artist 
+        @genre = genre 
+
+        @@count += 1
+        @@artists << artist 
+        @@genres << genre 
+    end
+
+    def self.count
+        return @@count 
+    end
+    
+    def self.artists 
+        @@artists.uniq
+    end
+
+    def self.genres 
+        @@genres.uniq
+    end 
+
+    def self.genre_count 
+        hash = {}
+        @@genres.each do |genre| 
+            if hash[genre] == nil
+                hash[genre] = 1
+            else
+                hash[genre] += 1
+            end
+        end
+
+        return hash 
+    end
+
+    def self.artist_count
+        hash = {}
+        @@artists.each do |artist| 
+            if hash[artist] == nil
+                hash[artist] = 1
+            else
+                hash[artist] += 1
+            end
+        end
+
+        return hash 
+    end
+end
